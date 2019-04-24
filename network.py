afa220cf7a41d8ad4a22deb5859468b741a6b56d
Gurucharan MK <charan4u@gmail.com>
2019-04-25 01:38:07 +0530

    Updated network.py with necessary functionalities

--- network.py
+++ network.py
@@ -2,25 +2,53 @@ import torch
 
 
 class Network (object):
-    def __init__(self, pretrained_arch, classifier, model):
-        self.pretrained_arch = pretrained_arch
-        self.classfier = classifier
+    def __init__(self, model):
         self.model = model
 
     def print_infreezed_layers(self):
-        pass
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                print("\t", name)
 
     def unfreeze_all_layers(self):
         pass
 
-    def freeze_pretrained_arch(self):
-        pass
-
     def get_model(self):
-        pass
+        return self.model
 
-    def load_model_from_checkpoint(self):
-        pass
+    def load_model_from_checkpoint(self, filename):
+        checkpoint = torch.load(filename, map_location='cpu')
+        self.model = checkpoint['arch']
+        self.model.classifier = checkpoint['classifier']
+        self.model.load_state_dict(checkpoint['state_dict'], strict=False)
+        optimizer = checkpoint['optimizer']
+        epochs = checkpoint['epochs']
+        scheduler = checkpoint['scheduler']
+        return optimizer, epochs, scheduler
 
-    def save_model_checkpoint(self):
-        pass
+    def save_model_checkpoint(
+            self,
+            filename,
+            input_size,
+            output_size,
+            epochs,
+            batch_size,
+            arch,
+            classifier,
+            sched,
+            optimizer,
+            model):
+        checkpoint = {
+            'input_size': input_size,
+            'output_size': output_size,
+            'epochs': epochs,
+            'batch_size': batch_size,
+            'arch': arch,
+            'classifier': classifier,
+            'scheduler': sched,
+            'optimizer': optimizer.state_dict(),
+            'state_dict': model.state_dict()
+        }
+        self.model = model
+        torch.save(checkpoint, filename)
+        return filename
